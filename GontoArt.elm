eea0d0b5b28f61a25523f2bbbafb17ac43454097
Gabriel Andretta <gabriel.andretta@gmail.com>
2016-06-20 16:19:34 -0300

    Render a box

--- GontoArt.elm
+++ GontoArt.elm
@@ -1,7 +1,20 @@
 module GontoArt exposing (..)
 
-import Html exposing (text)
-
+import Html exposing (div)
+import Html.Attributes exposing (style)
 
 main =
-  text "Hello"
+  box
+
+
+box =
+  div [ boxStyle ] []
+
+
+boxStyle =
+  style
+    [ ("width", "700px")
+    , ("height", "500px")
+    , ("margin", "50px")
+    , ("boxShadow", "0px 1px 4px 0px rgba(0,0,0,0.2)")
+    ]
