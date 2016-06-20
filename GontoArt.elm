c0e6b30228ceb69950b4d13d03c51b765db75538
Gabriel Andretta <gabriel.andretta@gmail.com>
2016-06-20 16:21:04 -0300

    Render a grid inside the box

--- GontoArt.elm
+++ GontoArt.elm
@@ -8,13 +8,37 @@ main =
 
 
 box =
-  div [ boxStyle ] []
+  div [ boxStyle ] rows
+
+
+cellLength = 30
+colCount = 15
+rowCount = 10
 
 
 boxStyle =
   style
-    [ ("width", "700px")
-    , ("height", "500px")
-    , ("margin", "50px")
+    [ ("margin", "50px")
     , ("boxShadow", "0px 1px 4px 0px rgba(0,0,0,0.2)")
+    , ("backgroundColor", "rgba(0,0,0,0.1)")
+    , ("position", "relative")
+    , ("width", (toString ((cellLength + 1) * colCount)) ++ "px")
+    , ("height", (toString ((cellLength + 1) * rowCount)) ++ "px")
+    ]
+
+
+rows =
+  [0..(rowCount - 1)]
+    |> List.concatMap ( \i -> List.map ( \j -> (i, j) ) [0..(colCount - 1)] )
+    |> List.map ( \ (i,j) -> div [ cellStyle i j ] [] )
+
+
+cellStyle i j =
+  style
+    [ ("position", "absolute")
+    , ("backgroundColor", "#fff")
+    , ("width", (toString cellLength) ++ "px")
+    , ("height", (toString cellLength) ++ "px")
+    , ("left", (toString (j * (cellLength + 1))) ++ "px")
+    , ("top", (toString (i * (cellLength + 1))) ++ "px")
     ]
